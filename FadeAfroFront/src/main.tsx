d7a79ecf89154be5b5c5f841f5d9aa2420d607d3
Peter <petrormanji68@mail.ru>
2026-05-02 12:28:26 +0300

    tg mini app library added

--- FadeAfroFront/src/main.tsx
+++ FadeAfroFront/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SDKProvider } from '@tma.js/sdk-react'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SDKProvider acceptCustomStyles debug>
+      <App />
+    </SDKProvider>
   </StrictMode>,
 )
