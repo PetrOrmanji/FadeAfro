46b160a1863de0f583d06c11d80f8798003f19d5
Peter <petrormanji68@mail.ru>
2026-05-03 23:29:45 +0300

    get users request updated

--- FadeAfro/FadeAfro.Application/Features/Users/GetUsers/GetUsersHandler.cs
+++ FadeAfro/FadeAfro.Application/Features/Users/GetUsers/GetUsersHandler.cs
@@ -1,10 +1,11 @@
 using FadeAfro.Application.Common;
+using FadeAfro.Application.Features.Users.Common;
 using FadeAfro.Domain.Repositories;
 using MediatR;
 
 namespace FadeAfro.Application.Features.Users.GetUsers;
 
-public class GetUsersHandler : IRequestHandler<GetUsersQuery, PagedResponse<GetUsersResponse>>
+public class GetUsersHandler : IRequestHandler<GetUsersQuery, PagedResponse<UserDto>>
 {
     private readonly IUserRepository _userRepository;
 
@@ -13,21 +14,26 @@ public class GetUsersHandler : IRequestHandler<GetUsersQuery, PagedResponse<GetU
         _userRepository = userRepository;
     }
 
-    public async Task<PagedResponse<GetUsersResponse>> Handle(GetUsersQuery query, CancellationToken cancellationToken)
+    public async Task<PagedResponse<UserDto>> Handle(GetUsersQuery query, CancellationToken cancellationToken)
     {
         var totalCount = await _userRepository.CountAsync(query.Search);
         var users = await _userRepository.GetAllAsync(query.Page, query.PageSize, query.Search);
+        
+        var userDtoList = new List<UserDto>();
 
-        var items = users
-            .Select(u => new GetUsersResponse(
-                u.Id,
-                u.TelegramId,
-                u.FirstName,
-                u.LastName,
-                u.Username,
-                u.Roles))
-            .ToList();
-
-        return new PagedResponse<GetUsersResponse>(items, totalCount, query.Page, query.PageSize);
+        foreach (var user in users)
+        {
+            var userDto = new UserDto(
+                user.Id,
+                user.TelegramId,
+                user.FirstName,
+                user.LastName,
+                user.Username,
+                user.Roles);
+            
+            userDtoList.Add(userDto);
+        }
+        
+        return new PagedResponse<UserDto>(userDtoList, totalCount, query.Page, query.PageSize);
     }
 }
--- FadeAfro/FadeAfro.Application/Features/Users/GetUsers/GetUsersQuery.cs
+++ FadeAfro/FadeAfro.Application/Features/Users/GetUsers/GetUsersQuery.cs
@@ -1,6 +1,7 @@
 using FadeAfro.Application.Common;
+using FadeAfro.Application.Features.Users.Common;
 using MediatR;
 
 namespace FadeAfro.Application.Features.Users.GetUsers;
 
-public record GetUsersQuery(int Page, int PageSize, string? Search) : IRequest<PagedResponse<GetUsersResponse>>;
+public record GetUsersQuery(int Page, int PageSize, string? Search) : IRequest<PagedResponse<UserDto>>;
