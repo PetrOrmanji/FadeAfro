2c417d18ba6fb39a5cfb699c1c864c8f75551e35
Peter <petrormanji68@mail.ru>
2026-05-05 16:44:46 +0300

    анимация загрузки

--- FadeAfroFront/src/components/LoadingScreen/LoadingScreen.tsx
+++ FadeAfroFront/src/components/LoadingScreen/LoadingScreen.tsx
@@ -2,12 +2,7 @@ import styles from './LoadingScreen.module.css'
 
 const LoadingScreen = () => (
   <div className={styles.page}>
-    <div className={styles.logo}>✂</div>
-    <div className={styles.dotsWrap}>
-      <span className={styles.dot} />
-      <span className={styles.dot} />
-      <span className={styles.dot} />
-    </div>
+    <div className={styles.circle} />
   </div>
 )
 
